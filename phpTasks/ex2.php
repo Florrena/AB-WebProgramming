3d8c101a97058de772dde0d0ef429c083e28c415
Florrena <ania.bar25@gmail.com>
2024-01-29 10:53:04 +0200

    task2

---
+++ phpTasks/ex2.php
@@ -0,0 +1,17 @@
+<?php 
+$title = 'TASK 2';
+include 'header.php'; ?>
+
+<body>
+
+        <div class="article"> 
+            <h2>Task 2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dolores unde maiores voluptates ullam sint ipsam suscipit maxime veniam optio qui quasi ratione ab facere, facilis temporibus assumenda modi minima.</p>
+        </div>
+
+
+
+
+</body>
+<?php include 'footer.php'; ?>
+</html>
---
+++ phpTasks/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <? echo $title; ?></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;1,100&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header class="header-container">
+        <div class="page-header">
+            <div class="logo"><a href="home.html"><img src="images/logo.png"></a></div>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="ex1.php">TASK 1</a></li>
+                    <li><a href="ex2.php">TASK 2</a></li>
+                    <li><a href="#">TASK 3</a></li>
+                    <li><a href="#">TASK 4</a></li>
+                    <li><a href="#">TASK 5</a></li>
+                    <li><a href="#">TASK 6</a></li>
+                    <li><a href="#">TASK 7</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
